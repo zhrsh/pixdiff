71ec2682f788c6e8e805477a669dadf1024ec9b5
zahra a.s <182934048+zhrsh@users.noreply.github.com>
2025-02-23 19:40:24 +0700

    Accept user arguments in main()

--- pixdiff.py
+++ pixdiff.py
@@ -1,14 +1,42 @@
 from PIL import Image
 import numpy as np
+import sys
+import os
 
 def main(): 
-    # load the two images
-    image1 = Image.open('image1.png').convert('RGBA')
-    image2 = Image.open('image2.png').convert('RGBA')
+    if len(sys.argv) != 3:
+        print("usage: pixdiff <image1> <image2>")
+        sys.exit(1)
 
-    # ensure both images are the same size
+    image1_path = sys.argv[1]
+    image2_path = sys.argv[2]
+    compare(image1_path, image2_path)
+
+def strip_path(path, include_extension=True):
+    # get base file name
+    file_name = os.path.basename(path)
+
+    if include_extension == False:
+        # strip file extension if include_extension is false:
+        main_file_name, _ = os.path.splitext(file_name)
+        return main_file_name
+    else:
+        # file name with extension
+        return file_name
+
+def compare(image1_path, image2_path):
+    # load the two images 
+    image1 = Image.open(image1_path).convert('RGBA')
+    image2 = Image.open(image2_path).convert('RGBA')
+
+    # check if images are the same dimensions (width, height)
     if image1.size != image2.size:
-        raise ValueError("Images must be the same size for comparison.")
+        raise ValueError("images must be the same size for comparison.")
+
+    # check if images are too large in dimension (width, height)
+    if image1.size[0] + image1.size[1] == 1024:
+        raise ValueError(f"the image {image1} is too large for comparison.")
+        # no need to do image2 because they have the same dimensions
 
     # convert images to NumPy arrays
     array1 = np.array(image1)
@@ -29,5 +57,12 @@ def main():
     # overlay the mask on the original image
     result = Image.alpha_composite(image1, mask)
 
-    # save or show the result
-    result.save('diff.png') 
+    # strip image1_path to base file with extension + _diff
+    file_name = f"{strip_path(image1_path, include_extension=False)}_diff.png"
+
+    # print info and save
+    print(f"diff saved as {file_name}")
+    result.save(file_name) 
+
+if __name__ == "__main__":
+    main()
