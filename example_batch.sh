35c4262d24b71f441df00053dd2875972c53c23b
zahra a.s <182934048+zhrsh@users.noreply.github.com>
2025-03-05 06:49:41 +0700

    Update example_batch.sh script

--- example_batch.sh
+++ example_batch.sh
@@ -13,11 +13,11 @@ pixdiff readme_images/example1.png readme_images/example2.png --path "readme_ima
 # only save mask
 pixdiff readme_images/example1.png readme_images/example2.png --path "readme_images/example_mask" --save-mask
 
-# use a custom alpha value
-pixdiff readme_images/example1.png readme_images/example2.png --path "readme_images/example_alpha" --alpha 250
+# use a custom rgba value
+pixdiff readme_images/example1.png readme_images/example2.png --path "readme_images/example_rgba" --rgba 94 48 235 128
 
-# only save mask and use a custom alpha value
-pixdiff readme_images/example1.png readme_images/example2.png --path "readme_images/example_mask_alpha" --save-mask --alpha 255
+# only save mask and use a custom rgba value
+pixdiff readme_images/example1.png readme_images/example2.png --path "readme_images/example_mask_rgba" --save-mask --rgba 94 48 235 128
 
 # save none and save csv
 pixdiff readme_images/example1.png readme_images/example2.png --path "readme_images/example_csv" --save-none --save-csv
