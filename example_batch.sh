2e7adc3acd9f90ead6dc1ad55cf58a0b41f69299
zahra a.s <182934048+zhrsh@users.noreply.github.com>
2025-04-22 13:36:05 +0700

    Tweak example_batch.sh for conciseness
    - cd into readme_images/ directory first
    - removes the need to specify the directory every time

--- example_batch.sh
+++ example_batch.sh
@@ -1,29 +1,31 @@
 #!/bin/zsh
 
+cd readme_images
+
 # export
-~/Applications/Aseprite.app/Contents/MacOS/aseprite -b --split-layers readme_images/example2.aseprite --scale 4 --save-as readme_images/{layer}.png
+~/Applications/Aseprite.app/Contents/MacOS/aseprite -b --split-layers example2.aseprite --scale 4 --save-as {layer}.png
 
 # ======================
 # pixdiff examples
 # ======================
 
 # default
-pixdiff readme_images/example1.png readme_images/example2.png --path "readme_images/example_default"
+pixdiff example1.png example2.png --path "example_default"
 
 # only save mask
-pixdiff readme_images/example1.png readme_images/example2.png --path "readme_images/example_mask" --save-mask
+pixdiff example1.png example2.png --path "example_mask" --save-mask
 
 # use a custom rgba value
-pixdiff readme_images/example1.png readme_images/example2.png --path "readme_images/example_rgba" --rgba 94 48 235 250
+pixdiff example1.png example2.png --path "example_rgba" --rgba 94 48 235 250
 
 # only save mask and use a custom rgba value
-pixdiff readme_images/example1.png readme_images/example2.png --path "readme_images/example_mask_rgba" --save-mask --rgba 94 48 235 250
+pixdiff example1.png example2.png --path "example_mask_rgba" --save-mask --rgba 94 48 235 250
 
 # use a color name without specifiying alpha
-pixdiff readme_images/example1.png readme_images/example2.png --path "readme_images/example_color" --color "hotpink"
+pixdiff example1.png example2.png --path "example_color" --color "hotpink"
 
 # use a color name with alpha
-pixdiff readme_images/example1.png readme_images/example2.png --path "readme_images/example_color_alpha" --color "hotpink" 255
+pixdiff example1.png example2.png --path "example_color_alpha" --color "hotpink" 255
 
 # save none and save csv
-pixdiff readme_images/example1.png readme_images/example2.png --path "readme_images/example_csv" --save-none --save-csv
+pixdiff example1.png example2.png --path "example_csv" --save-none --save-csv
